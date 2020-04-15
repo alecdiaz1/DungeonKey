06ab1807fed27bbe0b1e240b4d6cc204236cc5b0
Alec Diaz <alecdiaz1@yahoo.com>
2020-04-14 23:15:18 -0700

    create party model object

---
+++ src/me/alec/dungeonkey/Party.java
@@ -0,0 +1,35 @@
+package me.alec.dungeonkey;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class Party  {
+    public Player host;
+    public String dungeonName;
+    public ArrayList<Player> members;
+
+    public Player getHost() {
+        return host;
+    }
+
+    public void setHost(Player host) {
+        this.host = host;
+    }
+
+    public String getDungeonName() {
+        return dungeonName;
+    }
+
+    public void setDungeonName(String dungeonName) {
+        this.dungeonName = dungeonName;
+    }
+
+    public ArrayList<Player> getMembers() {
+        return members;
+    }
+
+    public void setMembers(ArrayList<Player> members) {
+        this.members = members;
+    }
+}
